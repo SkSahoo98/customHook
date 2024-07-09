6fdec388438e7eca80b62cc1a334c7521359ad79
SkSahoo98 <sahooamit642@gmail.com>
2024-07-09 19:07:23 +0530

    commit

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./card.module.css";
+import useFetch from "../Hooks/useFetch";
+
+const Card = () => {
+  const data = useFetch("https://fakestoreapi.com/users");
+
+  return (
+    <>
+      <h2>Welcome To User Profile!!!</h2>
+      <section>
+        {data &&
+          data.map((info, index) => {
+            return (
+              <div key={index} className={styles.card}>
+                <div className={styles.cardHeader}>
+                  <h4>{info.username}</h4>
+                </div>
+                <div className={styles.cardBody}>
+                  <div className={styles.cardTitle}>
+                    Name: {info.name.firstname} {info.name.lastname}
+                  </div>
+                  <div className={styles.cardText}>Email: {info.email}</div>
+                  <div className={styles.cardText}>
+                    Phone Number: {info.phone}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </section>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/Components/Hooks/useFetch.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return data;
+};
+
+export default useFetch;
